c4c1920213767234aadbadad47a9b95f75a3f837
Vinicius José <viniciusjosedev@gmail.com>
2023-06-14 15:28:51 -0300

    feat: requirement 11 done

--- app/backend/src/tests/change.me.test.ts
+++ app/backend/src/tests/change.me.test.ts
@@ -16,6 +16,8 @@ chai.use(chaiHttp);
 
 const { expect } = chai;
 
+const Authorization = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7ImVtYWlsIjoiYWRtaW5AYWRtaW4uY29tIiwidXNlcm5hbWUiOiJBZG1pbiIsImlkIjoxLCJyb2xlIjoiYWRtaW4ifSwiaWF0IjoxNjg2NzY1OTgyLCJleHAiOjE2ODczNzA3ODJ9.KsvL6NHWOKTjo_Z0vyfBbIpcgoS--Ayie6q421qg-2g'
+
 describe('All tests', () => {
   /**
    * Exemplo do uso de stubs com tipos
@@ -101,5 +103,32 @@ describe('All tests', () => {
 		});
 		
 		expect(result.status).to.be.deep.equal(400);
+  });
+
+	it('Return /login/role with sucess', async () => {
+		const result = await chai.request(app).get('/login/role')
+		.set('Authorization', Authorization).send();
+				
+		expect(result.status).to.be.deep.equal(200);
+  });
+
+	it('Return /login/role with fail', async () => {
+		const result = await chai.request(app).get('/login/role')
+		.set('Authorization', `${Authorization}f`).send();
+				
+		expect(result.status).to.be.deep.equal(401);
+  });
+
+	it('Return /login/role with fail', async () => {
+		const result = await chai.request(app).get('/login/role')
+		.set('Authorization', '').send();
+				
+		expect(result.status).to.be.deep.equal(401);
+  });
+
+	it('Return /login/role with fail', async () => {
+		const result = await chai.request(app).get('/login/role').send();
+				
+		expect(result.status).to.be.deep.equal(401);
   });
 });
