c1951564f671703d2f48da9f9362c35ccc1c5470
Vinicius José <viniciusjosedev@gmail.com>
2023-06-14 12:19:33 -0300

    feat: requirement 7 and 9

--- app/backend/src/tests/change.me.test.ts
+++ app/backend/src/tests/change.me.test.ts
@@ -9,6 +9,8 @@ import Example from '../database/models/ExampleModel';
 import { Response } from 'superagent';
 
 import resultTeamFindAll from './mocks/resultTeamFindAll';
+const Jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
 chai.use(chaiHttp);
 
@@ -41,6 +43,10 @@ describe('All tests', () => {
   //   expect(...)
   // });
 
+	afterEach(() => {
+		sinon.restore();
+	})
+
   it('Return teamFindAll', async () => {
 		const result = await chai.request(app).get('/teams');
 		
@@ -51,5 +57,42 @@ describe('All tests', () => {
 		const result = await chai.request(app).get('/teams/1');
 		
 		expect(result.body).to.be.deep.equal(resultTeamFindAll[0]);
+  });
+
+	it('Return userLogin sucess', async () => {
+		sinon.stub(bcrypt, 'compareSync').resolves(true)
+		sinon.stub(Jwt, "sign").resolves('123');
+		
+		const result = await chai.request(app).post('/login').send({
+			"password": "secret_admin",
+			"email": "admin@admin.com"
+		});
+		
+		expect(result.status).to.be.deep.equal(200);
+  });
+
+	it('Return userLogin faill with email wrong', async () => {
+		const result = await chai.request(app).post('/login').send({
+			"password": "secret_admin",
+			"email": "admin2@admin.com"});
+		
+		expect(result.status).to.be.deep.equal(400);
+  });
+
+	it('Return userLogin faill with password wrong', async () => {
+		const result = await chai.request(app).post('/login').send({
+			"password": "secret_admin2",
+			"email": "admin@admin.com"
+		});
+		
+		expect(result.status).to.be.deep.equal(400);
+  });
+
+	it('Return userLogin faill with email and password required', async () => {
+		const result = await chai.request(app).post('/login').send({
+			"password": "secret_admin2",
+		});
+		
+		expect(result.status).to.be.deep.equal(400);
   });
 });
